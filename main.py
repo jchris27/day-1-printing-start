26825af01dfdfbb501eb24e081ca91a3a7164636
Jin <53550425+jchris27@users.noreply.github.com>
2022-03-08 07:21:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Write your code below this line 👇
+print("Hello World!")
+
+# printing a new line using \n
+print("Hello World!\nHello World!")
+
+# string concatenation
+print("Hello" + " " + "Angela")
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello" + " " +  input("What is your name ") + "!")
+
+print(len(input("What is your name?")))
